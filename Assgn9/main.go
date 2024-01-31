38fb65038cfdd8c7ac9861fba6a2bf4239c3e34e
sharyu <sharayumarwadi11@gmail.com>
2024-01-31 22:32:19 +0530

    Assignment 9

---
+++ Assgn9/main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+type Websites struct {
+	List []string `json:"websites"`
+}
+
+var websiteMap map[string]string = make(map[string]string)
+var list []string
+
+func main() {
+	fmt.Println("Starting server")
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", genericHandler)
+
+	r.HandleFunc("/input", getWebsiteList).Methods("POST")
+
+	r.HandleFunc("/check", checkWebsiteStatus).Methods("GET")
+
+	go func() {
+		for {
+			if list != nil {
+				go backgroundJob(list)
+				time.Sleep(time.Minute)
+			}
+		}
+	}()
+
+	http.ListenAndServe("localhost:8080", r)
+
+}
+
+func getWebsiteList(w http.ResponseWriter, r *http.Request) {
+	var website Websites
+	err := json.NewDecoder(r.Body).Decode(&website)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	list = website.List
+
+	w.WriteHeader(http.StatusOK)
+
+}
+
+func backgroundJob(web []string) {
+	fmt.Println("running backgroundJob")
+	for _, data := range web {
+
+		response, err := http.Get("https://" + data)
+		if err == nil {
+			responseStatus := response.StatusCode
+			if responseStatus == 200 {
+				websiteMap[data] = "UP"
+			} else {
+				websiteMap[data] = "DOWN"
+			}
+
+		} else {
+			websiteMap[data] = "DOWN"
+		}
+	}
+}
+
+func checkWebsiteStatus(w http.ResponseWriter, r *http.Request) {
+
+	if len(websiteMap) == 0 {
+		w.WriteHeader(http.StatusNoContent)
+		w.Write([]byte("The map is empty"))
+		return
+	}
+
+	name := r.URL.Query().Get("name")
+
+	if name != "" {
+
+		_, ok := websiteMap[name]
+
+		if !ok {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("name not in the list"))
+			return
+		}
+
+		respJson, err := json.Marshal(map[string]string{name: websiteMap[name]})
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write(respJson)
+		return
+	}
+
+	respJson, err := json.Marshal(websiteMap)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(respJson)
+}
+
+func genericHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "welcome to the server, the page you are looking for does not exist!")
+}
